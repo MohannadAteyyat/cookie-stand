0f05d102c38e478891ca7859f7a7a78aa3afbb8b
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-05-03 02:31:01 +0300

    Solving step 3

--- js/app.js
+++ js/app.js
@@ -1,46 +1,67 @@
 'use strict';
-
 let seattleShop = {
     minhc: 23,
     maxhc: 65,
     aveCpc: 6.3,
-    numOfchour:0,
-    amofcookpufhr:0,
-
-    randomCustomer: function () {
-        this.numOfchour = randomCust(23, 65) + ' Customer';
-         console.log(seattleShop.numOfchour);
+    numOfchour: 0,
+    amofcookpufhr: 0,
+    resultsArray: [],
+  randomCustomer: function () {
+        this.numOfchour = randomCust(23, 65) ;
+        console.log(seattleShop.numOfchour + ' ' + 'Customer');   
     },
-
     amofcookpufhrfunction: function () {
-        
-        this.amofcookpufhr = gggggggg((randomCust(23, 65)) ,seattleShop.numOfchour) 
-        console.log(seattleShop.amofcookpufhr);
-       
+        seattleShop.amofcookpufhr = seattleShop.numOfchour* seattleShop.aveCpc;
+        console.log(seattleShop.amofcookpufhr + ' ' +'amounts of cookies purchased for each hour');  
+    }
+    // seattleShop.resultsArray=
 
-
-
-    },
-
-
+}
+function randomCust(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+    //The maximum is exclusive and the minimum is inclusive 
 }
 seattleShop.randomCustomer();
 seattleShop.amofcookpufhrfunction();
+
+
+
+
+// amofcookpufhrfunction: function () {
+
+//     this.amofcookpufhr = gggggggg((randomCust(23, 65)) ,seattleShop.numOfchour) 
+//     console.log(seattleShop.amofcookpufhr);
+
+
+// },
+
+
+
+
+// function amofcookpufhrfunction() {
+
+//     let amofcookpufhr = seattleShop.numOfchour * 6.3
+//     return amofcookpufhr
+
+
+// }
+
+
+// console.log(seattleShop.amofcookpufhr)
+
+// seattleShop.gggggggg();
+// seattleShop.amofcookpufhrfunction();
 // seattleShop.amofcookpufhrfunction();
 
-function gggggggg(j,u) {
-    return j * u
-    
-}
+
+// function gggggggg() {
+//     let a = this.numOfchour * this.aveCpc
+// return a;}
+
+// }
 
 
 
-function randomCust(min, max) {
-
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    //The maximum is exclusive and the minimum is inclusive
-
-}
 
 
 
@@ -50,7 +71,7 @@ function randomCust(min, max) {
 //  numOfchour = seattleShop.randomCustomer();
 //  seattleShop.amofcookpufhr = amofcookpufhrfunction();
 //  console.log(seattleShop.numOfchour);
-console.log(seattleShop.amofcookpufhr);
+
 
 
 
